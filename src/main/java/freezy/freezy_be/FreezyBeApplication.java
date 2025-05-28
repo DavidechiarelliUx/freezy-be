f57f6757c0928c183da6797088798898222df500
Davide Chiarelli <chiarellidavide99@gmail.com>
2025-05-28 11:23:24 +0200

    Initial commit

---
+++ src/main/java/freezy/freezy_be/FreezyBeApplication.java
@@ -0,0 +1,13 @@
+package freezy.freezy_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreezyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreezyBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/freezy/freezy_be/FreezyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package freezy.freezy_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreezyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
